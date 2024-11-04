198ffdbf16994fe21c6ab55a1407d6c2c07904b0
GGGGXQ <19860599289@163.com>
2024-11-04 23:09:01 +0800

    change file

--- Searching/bfs_carching.py
+++ Searching/bfs_carching.py
@@ -1,10 +1,22 @@
+# example_input:
+# 4 4
+# 2 2 4 4
+# 2
+# 1 1
+# 1 4
+# ....
+# #.##
+# ....
+# ....
+# example_output:
+# YES
 from collections import deque
 
 
 def bfs(n, m, tx, ty, board, directions):
-    q = deque([(tx-1, ty-1)])
-    visited = {(tx - 1, ty - 1)}
-    distance = {(tx - 1, ty - 1): 0}
+    q = deque([(tx - 1, ty - 1)])
+    distance = [[0 for _ in range(m)] for _ in range(n)]
+    visited = [[0 for _ in range(m)] for _ in range(n)]
 
     while q:
         cx, cy = q.popleft()
@@ -13,11 +25,10 @@ def bfs(n, m, tx, ty, board, directions):
             if 0 <= cx_to_tx < n and 0 <= cy_to_ty < m:
                 if board[cx_to_tx][cy_to_ty] == '#':
                     continue
-                if (cx_to_tx - 1, cy_to_ty - 1) not in visited:
-                    visited.add((cx_to_tx, cy_to_ty))
-                    distance[(cx_to_tx, cy_to_ty)] = distance[(cx, cy)] + 1
+                if visited[cx_to_tx][cy_to_ty] == 0:
                     q.append((cx_to_tx, cy_to_ty))
-
+                    distance[cx_to_tx][cy_to_ty] = distance[cx][cy] + 1
+                    visited[cx_to_tx][cy_to_ty] = 1
     return distance
 
 
@@ -26,15 +37,14 @@ def main():
     sx, sy, tx, ty = map(int, input().split())
     k = int(input())
     monster = [tuple(map(int, input().split())) for _ in range(k)]
-    board = [[' ' for _ in range(m)] for _ in range(n)]
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
+    board = [['' for _ in range(m)] for _ in range(n)]
     for i in range(n):
-        board[i] = list(input().strip())
+        board[i] = input().strip()
 
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     distance = bfs(n, m, tx, ty, board, directions)
     for mx, my in monster:
-        if distance[(mx - 1, my - 1)] < distance[(sx - 1, sy - 1)]:
+        if distance[mx - 1][my - 1] <= distance[sx - 1][sy - 1]:
             print("NO")
             return
     print("YES")
